4ec474e1314086a54083d15a4bf433dac38fce3d
unknown <danielnaswan>
2024-08-28 11:25:36 +0800

    test

---
+++ application/logs/log-2024-08-27.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2024-08-27 17:58:37 --> Severity: Warning  --> mysqli_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp5\htdocs\PaySabil\system\database\drivers\mysqli\mysqli_driver.php 76
+ERROR - 2024-08-27 17:58:37 --> Unable to connect to the database
+ERROR - 2024-08-27 17:59:14 --> Severity: Warning  --> mysqli_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp5\htdocs\PaySabil\system\database\drivers\mysqli\mysqli_driver.php 76
+ERROR - 2024-08-27 17:59:14 --> Unable to connect to the database
+ERROR - 2024-08-27 18:02:33 --> Severity: Warning  --> mysqli_connect(): (HY000/2002): A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.
+ C:\xampp5\htdocs\PaySabil\system\database\drivers\mysqli\mysqli_driver.php 76
+ERROR - 2024-08-27 18:02:33 --> Unable to connect to the database
