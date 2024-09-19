dd9436842fe48b6bf6d3ea67714e50fe7c73727d
danielnaswan <danielnaswan03@gmail.com>
2024-09-19 14:31:24 +0800

    added the qrcode library

---
+++ application/logs/log-2024-09-12.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2024-09-12 05:30:34 --> 404 Page Not Found --> CafeController/assets
+ERROR - 2024-09-12 05:30:38 --> 404 Page Not Found --> CafeController/assets
+ERROR - 2024-09-12 05:30:40 --> 404 Page Not Found --> CafeController/assets
+ERROR - 2024-09-12 05:30:41 --> 404 Page Not Found --> CafeController/assets
+ERROR - 2024-09-12 05:30:45 --> 404 Page Not Found --> CafeController/assets
